39935bac131867d6591fe6899b0c58db06ed3aba
marcostobias <marcostobias64@gmail.com>
2022-04-11 13:32:40 +0200

    Bulk indexing implemented. Still some documents are missing and indexing time is too slow.

--- src/main/java/co/empathy/academy/search/utils/ReadDataUtils.java
+++ src/main/java/co/empathy/academy/search/utils/ReadDataUtils.java
@@ -1,20 +1,42 @@
 package co.empathy.academy.search.utils;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.BulkRequest;
+import co.elastic.clients.elasticsearch.core.IndexRequest;
+import co.elastic.clients.elasticsearch.core.bulk.BulkOperation;
+import co.elastic.clients.elasticsearch.core.bulk.IndexOperation;
+import co.elastic.clients.json.JsonpMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import jakarta.json.Json;
 import jakarta.json.JsonObject;
+import jakarta.json.stream.JsonParser;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ReadDataUtils {
     private static final ElasticsearchClient client = ElasticUtils.getClient();
 
     public static void getData(String filePath) {
         try(BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            reader.readLine();
-            String currentLine;
-            while((currentLine = reader.readLine()) != null) {
-                index(currentLine);
+            String currentLine = reader.readLine();
+
+            while(currentLine != null) {
+                Map<String, JsonObject> bulkOps = new HashMap<>();
+                for(int i = 0; i < 1000; i++) {
+                    currentLine = reader.readLine();
+
+                    if(currentLine != null)
+                        bulkOps.putAll(getJson(currentLine));
+                    else
+                        break;
+                }
+
+
+                index(bulkOps);
             }
         } catch(Exception e) {
             e.printStackTrace();
@@ -22,10 +44,12 @@ public class ReadDataUtils {
     }
 
 
-    private static void index(String line) {
+    private static Map<String, JsonObject> getJson(String line) {
         String[] fields = line.split("\t");
+        Map<String, JsonObject> element = new HashMap<>();
 
-        JsonObject json = Json.createObjectBuilder()
+
+        element.put(fields[0], Json.createObjectBuilder()
                 .add("titleType", fields[1])
                 .add("primaryTitle", fields[2])
                 .add("originalTitle", fields[3])
@@ -34,14 +58,24 @@ public class ReadDataUtils {
                 .add("endYear", StringToIntConverter.getInt(fields[6]))
                 .add("runtimeMinutes", StringToIntConverter.getInt(fields[7]))
                 .add("genres", fields[8])
-                .build();
+                .build());
 
-
-        Reader queryJson = new StringReader(json.toString());
-
-
+        return element;
+    }
+    private static void index(Map<String, JsonObject> bulkOps) {
         try {
-            client.index(builder -> builder.index("films").id(fields[0]).withJson(queryJson));
+            client.bulk(_0 -> _0
+                    .operations(bulkOps.keySet().stream().map(x ->
+                            BulkOperation.of(_1 -> _1
+                                    .index(_2 -> _2
+                                            .index("films")
+                                            .id(x)
+                                            .document(bulkOps.get(x))
+                                    )
+                            )
+                    ).toList())
+            );
+            //client.index(builder -> builder.index("films").id(fields[0]).withJson(queryJson));
         } catch (IOException e) {
             e.printStackTrace();
         }
