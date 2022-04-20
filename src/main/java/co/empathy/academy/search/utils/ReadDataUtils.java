dfc39e05dcbed0004e32106f358da4d9697f93a8
marcostobias <marcostobias64@gmail.com>
2022-04-20 10:26:58 +0200

    Reset to previous commit 2nd try

--- src/main/java/co/empathy/academy/search/utils/ReadDataUtils.java
+++ src/main/java/co/empathy/academy/search/utils/ReadDataUtils.java
@@ -11,16 +11,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * Used for reading data from the file specified on batches
  */
 public class ReadDataUtils {
-    private final int MAX_THREADS = 3;
+    private final ElasticsearchClient client = ElasticUtils.getClient();
     //Size of the batches
-    private int batchSize = 50000;
+    private int batchSize = 100000;
     //Reader for the file
     private String filePath = "";
 
@@ -43,31 +41,67 @@ public class ReadDataUtils {
     }
 
     /**
+     * Returns a JsonContent consisting on the id of the specified line, and the value
+     * being a JSON with the different fields of the line parsed
+     * @param line containing the document that we want to index
+     * @return a JsonContent with the id and the document in JSON format
+     */
+    private JsonContent getJson(String line) {
+        String[] fields = line.split("\t");
+
+
+        return new JsonContent(fields[0], Json.createObjectBuilder()
+                .add("titleType", fields[1])
+                .add("primaryTitle", fields[2])
+                .add("originalTitle", fields[3])
+                .add("isAdult", StringToBoolConverter.getBool(fields[4]))
+                .add("startYear", StringToIntConverter.getInt(fields[5]))
+                .add("endYear", StringToIntConverter.getInt(fields[6]))
+                .add("runtimeMinutes", StringToIntConverter.getInt(fields[7]))
+                .add("genres", fields[8])
+                .build());
+    }
+
+    /**
      * Index the data
      */
     public void indexData() {
-        List<String> lines = null;
         try {
-            lines = Files.readAllLines(Path.of(this.filePath));
-        } catch (IOException e) {
-            throw new InternalServerException("There was a problem reading the file you provided", e);
+            List<String> lines = Files.readAllLines(Path.of(this.filePath));
+
+            long current = 1;
+            long localBatch = this.batchSize;
+
+            if(this.batchSize > lines.size()) localBatch = lines.size() - 1L;
+
+            while(current < lines.size() - 1L) {
+                if (current + batchSize > lines.size())
+                    localBatch = lines.size() - current;
+
+                long finalCurrent = current;
+                long finalLocalBatch = localBatch;
+
+                client.bulk(_0 -> _0
+                        .operations(lines.stream().skip(finalCurrent).limit(finalLocalBatch)
+                                .map(this::getJson)
+                                .map(x ->
+                                        BulkOperation.of(_1 -> _1
+                                                .index(_2 -> _2
+                                                        .index("films")
+                                                        .id(x.id)
+                                                        .document(x.json)
+                                                )
+                                        )
+                                ).toList())
+                );
+
+
+                System.out.println("Indexed");
+                current += batchSize;
+            }
+        } catch(IOException | ElasticsearchException e) {
+            throw new InternalServerException("There was a problem processing your request", e);
         }
-
-        int workersSubListSizes = lines.size() / MAX_THREADS;
-
-        ExecutorService pool = Executors.newFixedThreadPool(MAX_THREADS);
-
-        int workerStartCounter = 1;
-
-        for(int i = 0; i < MAX_THREADS; i++) {
-            if(i == MAX_THREADS - 1)
-                pool.execute(new WorkerBulkIndexer(lines.subList(workerStartCounter, lines.size()), this.batchSize));
-            else
-                pool.execute(new WorkerBulkIndexer(lines.subList(workerStartCounter, workerStartCounter + workersSubListSizes), this.batchSize));
-            workerStartCounter+= workersSubListSizes;
-        }
-
-        pool.shutdown();
     }
 
     private static class JsonContent {
@@ -77,76 +111,6 @@ public class ReadDataUtils {
         public JsonContent(String id, JsonObject json) {
             this.id = id;
             this.json = json;
-        }
-    }
-
-    private static class WorkerBulkIndexer implements Runnable {
-        private final ElasticsearchClient client = ElasticUtils.getClient();
-        private List<String> lines = null;
-        private int batchSize = 0;
-        public WorkerBulkIndexer(List<String> lines, int batchSize) {
-            this.lines = lines;
-            this.batchSize = batchSize;
-        }
-
-        /**
-         * Returns a JsonContent consisting on the id of the specified line, and the value
-         * being a JSON with the different fields of the line parsed
-         * @param line containing the document that we want to index
-         * @return a JsonContent with the id and the document in JSON format
-         */
-        private JsonContent getJson(String line) {
-            String[] fields = line.split("\t");
-
-
-            return new JsonContent(fields[0], Json.createObjectBuilder()
-                    .add("titleType", fields[1])
-                    .add("primaryTitle", fields[2])
-                    .add("originalTitle", fields[3])
-                    .add("isAdult", StringToBoolConverter.getBool(fields[4]))
-                    .add("startYear", StringToIntConverter.getInt(fields[5]))
-                    .add("endYear", StringToIntConverter.getInt(fields[6]))
-                    .add("runtimeMinutes", StringToIntConverter.getInt(fields[7]))
-                    .add("genres", fields[8])
-                    .build());
-        }
-
-        @Override
-        public void run() {
-            try {
-                long current = 0;
-                long localBatch = this.batchSize;
-
-                if(this.batchSize > lines.size()) localBatch = lines.size() - 1L;
-
-                while(current < lines.size() - 1L) {
-                    if (current + batchSize > lines.size())
-                        localBatch = lines.size() - current;
-
-                    long finalCurrent = current;
-                    long finalLocalBatch = localBatch;
-
-                    client.bulk(_0 -> _0
-                            .operations(lines.stream().skip(finalCurrent).limit(finalLocalBatch)
-                                    .map(this::getJson)
-                                    .map(x ->
-                                            BulkOperation.of(_1 -> _1
-                                                    .index(_2 -> _2
-                                                            .index("films")
-                                                            .id(x.id)
-                                                            .document(x.json)
-                                                    )
-                                            )
-                                    ).toList())
-                    );
-
-
-                    System.out.println("Indexed");
-                    current += batchSize;
-                }
-            } catch(IOException | ElasticsearchException e) {
-                throw new InternalServerException("There was a problem processing your request", e);
-            }
         }
     }
 }
